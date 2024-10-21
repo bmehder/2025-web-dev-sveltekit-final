f561a461cc8c3bc59bbe91abbb1962f18b6c34d5
bmehder <bmehder@gmail.com>
2024-10-21 22:56:48 +0100

    Hero

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to </h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import Hero from '../lib/Hero.svelte';
+
+</script>
+
+<Hero />
