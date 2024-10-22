2e9d516a64db3d4d2de47d760a258a837f38660d
bmehder <bmehder@gmail.com>
2024-10-22 18:37:35 +0100

    displayed posts on blog page

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -5,3 +5,16 @@
 </script>
 
 <Title name="Blog" />
+
+<section>
+  <div class="outer">
+    <div class="inner auto-fit">
+      {#each data.posts as post}
+        <div class="flow">
+          <h2>{post.title}</h2>
+          <p>{post.body}</p>
+        </div>
+      {/each}
+    </div>
+  </div>
+</section>
