b22a8bce6700b76f586572035f3746d6c71726ed
bmehder <bmehder@gmail.com>
2024-10-22 16:35:22 +0100

    v1 of homepage

---
+++ src/lib/Box.svelte
@@ -0,0 +1,35 @@
+<script>
+  let {
+    heading,
+    src,
+    description = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, dolor.",
+    href
+  } = $props()
+</script>
+
+<div class="flow bg-white">
+  <img {src} alt="">
+  <h3>{heading}</h3>
+  <p>{description}</p>
+  <a {href}>Read More &raquo;</a>
+</div>
+
+<style>
+  div {
+    position: relative;
+    padding: 2rem;
+    border-radius: 1rem;
+    font-size: 1.125rem;
+  }
+
+  img {
+    position: absolute;
+    max-width: 3.5rem;
+    top: -1.5rem;
+  }
+
+  a {
+    display: inline-block;
+    font-weight: bold;
+  }
+</style>
---
+++ src/lib/HeadingBox.svelte
@@ -0,0 +1,16 @@
+<script>
+  let { heading } = $props()
+</script>
+
+<div class="bg-gradient-pink white">
+  <h2>{heading}</h2>
+</div>
+
+<style>
+  div {
+    padding-block: 4rem;
+    padding-inline: 2rem;
+    border-radius: 1rem;
+    font-size: 1.25rem;
+  }
+</style>
