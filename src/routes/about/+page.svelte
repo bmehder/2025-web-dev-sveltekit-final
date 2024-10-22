252a5d7736bad6a70013cb9cc9fbc970a15f67a9
bmehder <bmehder@gmail.com>
2024-10-22 17:11:37 +0100

    v1 of about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -3,3 +3,56 @@
 </script>
 
 <Title name="About" />
+
+<section>
+	<div class="outer">
+		<div class="inner flow">
+			<h2>Subheading One</h2>
+
+			<div class="columns flow">
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis qui iste aliquid veritatis repudiandae! Delectus minus facere ut iure numquam voluptate quos. Incidunt harum cupiditate nulla ducimus a aliquid numquam, vel quaerat!</p>
+
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, eaque rem in voluptatem nemo facilis saepe obcaecati aperiam, modi earum deleniti officia optio veritatis eligendi vero temporibus, dicta eveniet! Necessitatibus vero eligendi, animi itaque illum sequi! Quis sequi neque id inventore tenetur?</p>
+
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, a, ipsa maxime voluptate magni enim in quisquam hic nemo beatae sit, eius similique. Odio, culpa dolor rem explicabo quos fuga at sequi velit expedita natus quia qui aperiam omnis officiis. Magni similique dolor iure cupiditate, voluptate eum nisi perspiciatis facere et ipsam.</p>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section>
+	<div class="outer bg-blue white">
+		<div class="inner flow">
+			<h2>Subheading Two</h2>
+
+			<div class="columns flow">
+				<p class="white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis qui iste aliquid veritatis repudiandae! Delectus minus facere ut iure numquam voluptate quos. Incidunt harum cupiditate nulla ducimus a aliquid numquam, vel quaerat!</p>
+
+				<p class="white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, eaque rem in voluptatem nemo facilis saepe obcaecati aperiam, modi earum deleniti officia optio veritatis eligendi vero temporibus, dicta eveniet! Necessitatibus vero eligendi, animi itaque illum sequi! Quis sequi neque id inventore tenetur?</p>
+
+				<p class="white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, a, ipsa maxime voluptate magni enim in quisquam hic nemo beatae sit, eius similique. Odio, culpa dolor rem explicabo quos fuga at sequi velit expedita natus quia qui aperiam omnis officiis. Magni similique dolor iure cupiditate, voluptate eum nisi perspiciatis facere et ipsam.</p>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section>
+	<div class="outer">
+		<div class="inner flow wide-text">
+			<h2>Subheading Three</h2>
+				<img class="float-right" src="https://picsum.photos/500/300" alt="">
+
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis qui iste aliquid veritatis repudiandae! Delectus minus facere ut iure numquam voluptate quos. Incidunt harum cupiditate nulla ducimus a aliquid numquam, vel quaerat!</p>
+
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, eaque rem in voluptatem nemo facilis saepe obcaecati aperiam, modi earum deleniti officia optio veritatis eligendi vero temporibus, dicta eveniet! Necessitatibus vero eligendi, animi itaque illum sequi! Quis sequi neque id inventore tenetur?</p>
+
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, a, ipsa maxime voluptate magni enim in quisquam hic nemo beatae sit, eius similique. Odio, culpa dolor rem explicabo quos fuga at sequi velit expedita natus quia qui aperiam omnis officiis. Magni similique dolor iure cupiditate, voluptate eum nisi perspiciatis facere et ipsam. Odio, culpa dolor rem explicabo quos fuga at sequi velit expedita natus quia qui aperiam omnis officiis. Magni similique dolor iure cupiditate, voluptate eum nisi perspiciatis facere et ipsam.</p>
+		</div>
+	</div>
+</section>
+
+<style>
+	.wide-text p {
+		max-width: unset;
+	}
+</style>
