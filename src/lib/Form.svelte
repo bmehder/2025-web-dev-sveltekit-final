0a1a3aa4aedd1d953b6e6fb4097907d27517379a
bmehder <bmehder@gmail.com>
2024-10-22 17:54:51 +0100

    v1 of contact page

---
+++ src/lib/Form.svelte
@@ -0,0 +1,83 @@
+<form>
+  <div class="form-wrapper">
+    <div class="form-item">
+      <label for="first">First Name</label>
+      <input 
+        type="text"
+        id="first"
+        name="first"
+        placeholder="Enter first name..."
+        required
+      />
+    </div>
+    <div class="form-item">
+      <label for="last">Last Name</label>
+      <input 
+        type="text"
+        id="last"
+        name="last"
+        placeholder="Enter last name..."
+        required
+      />
+    </div>
+    <div class="form-item">
+      <label for="email">Email</label>
+      <input 
+        type="email"
+        id="email"
+        name="email"
+        placeholder="Enter email address..."
+        required
+      />
+    </div>
+    <div class="form-item">
+      <label for="tel">Phone (Optional)</label>
+      <input 
+        type="tel"
+        id="tel"
+        name="tel"
+        placeholder="Enter phone number..."
+      />
+    </div>
+    <div class="form-item span-2">
+      <label for="message">Message</label>
+      <textarea name="message" id="message" rows="10"></textarea>
+    </div>
+    <button type="submit" class="bg-blue white">Submit Message</button>
+  </div>
+</form>
+
+<style>
+  .form-wrapper {
+    max-width: 48rem;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    align-items: start;
+    gap: 1.5rem;
+  }
+
+  .form-item {
+    display: grid;
+  }
+
+  .span-2 {
+    grid-column: span 2;
+  }
+
+  label {
+    font-weight: 600;
+  }
+
+  input, textarea {
+    padding: 1rem;
+    font: inherit;
+  }
+
+  button {
+    justify-self: start;
+  }
+
+  button:hover {
+    background-image: var(--gradient-pink);
+  }
+</style>
