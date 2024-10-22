ecd067ba4f37a272c2bb247bc3764a04c52c6b15
bmehder <bmehder@gmail.com>
2024-10-22 19:59:39 +0100

    changed Header button color

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,36 +1,40 @@
 <header>
-  <div class="outer">
-    <div class="inner spread-apart">
-      <a href="/"><img src="/logo-dark.svg" alt="dark logo"></a>
-      <nav>
-        <ul class="spread-apart">
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a href="/about">About</a>
-          </li>
-          <li>
-            <a href="/blog">Blog</a>
-          </li>
-          <li>
-            <a href="/contact">Contact</a>
-          </li>
-          <li>
-            <a href="/blog" class="button bg-gradient-pink white">Get Started</a>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  </div>
+	<div class="outer">
+		<div class="inner spread-apart">
+			<a href="/"><img src="/logo-dark.svg" alt="dark logo" /></a>
+			<nav>
+				<ul class="spread-apart">
+					<li>
+						<a href="/">Home</a>
+					</li>
+					<li>
+						<a href="/about">About</a>
+					</li>
+					<li>
+						<a href="/blog">Blog</a>
+					</li>
+					<li>
+						<a href="/contact">Contact</a>
+					</li>
+					<li>
+						<a href="/blog" class="button bg-blue white">Get Started</a>
+					</li>
+				</ul>
+			</nav>
+		</div>
+	</div>
 </header>
 
 <style>
-  .inner {
-    padding-block: 1.5rem;
-  }
+	.inner {
+		padding-block: 1.5rem;
+	}
 
-  ul {
-    list-style: none;
-  }
+	ul {
+		list-style: none;
+	}
+
+	.button:hover {
+		background-image: var(--gradient-pink);
+	}
 </style>
