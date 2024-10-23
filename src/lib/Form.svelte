a5c9081260d9a9a39ea3443dfe076732c9e44854
bmehder <bmehder@gmail.com>
2024-10-23 16:24:24 +0100

    media query contact form

--- src/lib/Form.svelte
+++ src/lib/Form.svelte
@@ -1,83 +1,85 @@
 <form>
-  <div class="form-wrapper">
-    <div class="form-item">
-      <label for="first">First Name</label>
-      <input 
-        type="text"
-        id="first"
-        name="first"
-        placeholder="Enter first name..."
-        required
-      />
-    </div>
-    <div class="form-item">
-      <label for="last">Last Name</label>
-      <input 
-        type="text"
-        id="last"
-        name="last"
-        placeholder="Enter last name..."
-        required
-      />
-    </div>
-    <div class="form-item">
-      <label for="email">Email</label>
-      <input 
-        type="email"
-        id="email"
-        name="email"
-        placeholder="Enter email address..."
-        required
-      />
-    </div>
-    <div class="form-item">
-      <label for="tel">Phone (Optional)</label>
-      <input 
-        type="tel"
-        id="tel"
-        name="tel"
-        placeholder="Enter phone number..."
-      />
-    </div>
-    <div class="form-item span-2">
-      <label for="message">Message</label>
-      <textarea name="message" id="message" rows="10"></textarea>
-    </div>
-    <button type="submit" class="bg-blue white">Submit Message</button>
-  </div>
+	<div class="form-wrapper">
+		<div class="form-item">
+			<label for="first">First Name</label>
+			<input
+				type="text"
+				id="first"
+				name="first"
+				placeholder="Enter first name..."
+				required
+			/>
+		</div>
+		<div class="form-item">
+			<label for="last">Last Name</label>
+			<input
+				type="text"
+				id="last"
+				name="last"
+				placeholder="Enter last name..."
+				required
+			/>
+		</div>
+		<div class="form-item">
+			<label for="email">Email</label>
+			<input
+				type="email"
+				id="email"
+				name="email"
+				placeholder="Enter email address..."
+				required
+			/>
+		</div>
+		<div class="form-item">
+			<label for="tel">Phone (Optional)</label>
+			<input type="tel" id="tel" name="tel" placeholder="Enter phone number..." />
+		</div>
+		<div class="form-item span-2">
+			<label for="message">Message</label>
+			<textarea name="message" id="message" rows="10"></textarea>
+		</div>
+		<button type="submit" class="bg-blue white">Submit Message</button>
+	</div>
 </form>
 
 <style>
-  .form-wrapper {
-    max-width: 48rem;
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    align-items: start;
-    gap: 1.5rem;
-  }
+	.form-wrapper {
+		max-width: 48rem;
+		display: grid;
+		align-items: start;
+		gap: 1.5rem;
 
-  .form-item {
-    display: grid;
-  }
+		@media (width > 32rem) {
+			grid-template-columns: 1fr 1fr;
+		}
+	}
 
-  .span-2 {
-    grid-column: span 2;
-  }
+	.form-item {
+		display: grid;
+	}
 
-  label {
-    font-weight: 600;
-  }
+	.span-2 {
+		@media (width > 32rem) {
+			grid-column: span 2;
+		}
+	}
 
-  input, textarea {
-    padding: 1rem;
-    font: inherit;
-  }
+	label {
+		font-weight: 600;
+	}
 
-  button {
-    justify-self: start;
-  }
+	input,
+	textarea {
+		min-width: 0;
+		padding: 1rem;
+		font: inherit;
+	}
 
-  button:hover {
-    background-image: var(--gradient-pink);
-  }
+	button {
+		justify-self: start;
+	}
+
+	button:hover {
+		background-image: var(--gradient-pink);
+	}
 </style>
